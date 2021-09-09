fd9a8bed6612f5f8cc599109ec095c45a49563c5
TaraMKelly <tara.kelly16@gmail.com>
2021-09-09 17:02:25 -0400

    commit tree 6ec85cda6156d6066029947fe4404a6788f5554e

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,18 @@
 class RecipesController < ApplicationController
 
     def index
-        recipes = Recipe.all 
-        render json: recipes, status: :ok
+        render json: Recipe.all
     end
+
+    def create
+        recipe = @current_user.recipes.create!(recipe_params)
+        render json: recipe, status: :created
+    end
+    
+    private
+    
+    def recipe_params
+        params.permit(:title, :instructions, :minutes_to_complete)
+    end
+
 end
